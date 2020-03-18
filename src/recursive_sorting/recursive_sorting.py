5144faf4ae35bfdebdb75e1afc4c4141579f44c5
alxanderpierre <alxanderpierre@users.noreply.github.com>
2020-03-18 15:22:11 -0400

    updata

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -2,43 +2,81 @@
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
-    # TO-DO
+    a = 0
+    b = 0
+
+
+    for k in range(0, elements):
+        if a >= len(arrA):
+            merged_arr[k] = arrB[b]
+            b+=1
+
+        elif b >= len(arrB):
+            merged_arr[k]= arrA[a]
+            a += 1
+        elif arrA[a] < arrB[b]:
+            a+=1
+        else:
+            merged_arr[k] = arrB[b]
+            b+=1
 
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
-  print(f"Slitting, {nlist}")
-  if len(nlist) >1:
+def merge_sort(arr):
+    if len(arr) >1:
+        left = merge_sort(arr[0 : len(arr) //2])
+        right = merge_sort( arr[len(arr) //2:])
 
-    mid = len(nlist) //2
-    lefthalf = nlist[:mid]
-    righthalf = nlist[mid:]
+        arr = merge(left, right)
+
+    return arr
+
+
+
+
+
+
+
+
+
+
+
+
+
+# this is doing merge sort in one function
+def mergeSort( arr ):
+  print(f"Slitting, {arr}")
+  if len(arr) >1:
+
+    mid = len(arr) //2
+    lefthalf = arr[:mid]
+    righthalf = arr[mid:]
 
     merge_sort(lefthalf)
     merge_sort(righthalf)
     i=j=k=0
     while i < len(lefthalf) and j < len(righthalf):
       if lefthalf[i] < righthalf[j]:
-        nlist[k] = lefthalf[i]
+        arr[k] = lefthalf[i]
         i=i+1
       else:
-        nlist[k] = righthalf[j]
+        arr[k] = righthalf[j]
         j=j+1
       k=k+1
 
     while i < len(lefthalf):
-      nlist[k] = lefthalf[i]
+      arr[k] = lefthalf[i]
       i=i+1
       k=k+1
 
     while j < len(righthalf):
-      nlist[k] = righthalf[j]
+      arr[k] = righthalf[j]
       j=j+1
       k=k+1
 
-    print(f'Merging {nlist}')
+    print(f'Merging {arr}')
 
 
     return arr
